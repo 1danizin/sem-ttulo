145c91aa13a9162f61fefc7424bb94ee91db80d8
1danizin <114154473+1danizin@users.noreply.github.com>
2022-09-22 08:48:49 -0300

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("vamos lá!")
+    basic.pause(randint(2000, 9000))
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+	
+})
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+basic.forever(function () {
+	
+})
