ee280064c0ebe629b5387995b483f6e2818c8cc1
1danizin <114154473+1danizin@users.noreply.github.com>
2022-10-06 08:48:44 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -2,16 +2,43 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
-    basic.showString("vamos lá!")
     basic.pause(randint(2000, 9000))
     basic.showIcon(IconNames.Yes)
     basic.pause(1000)
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P2, function () {
-	
+    if (Tocar == 0) {
+        _2 += 1
+        basic.showLeds(`
+            . . # # #
+            . . # . #
+            # # # # #
+            # # . . #
+            # # . . #
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
+    if (Tocar == 0) {
+        _1 += 1
+        basic.showLeds(`
+            # # # . #
+            # . # . .
+            # # # . #
+            # . . . #
+            # . . . #
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+let Tocar = 0
+let _1 = 0
+let _2 = 0
+basic.forever(function () {
 	
 })
 basic.forever(function () {
